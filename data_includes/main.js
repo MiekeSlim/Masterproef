3f633223c23473f5591119bccc1bc53f9c82ea41
HeleneVanMarcke <62547041+HeleneVanMarcke@users.noreply.github.com>
2020-09-04 16:13:27 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -101,15 +101,15 @@ PennController("DeviceCheck+Subject",
 //               .shuffle()
                 .settings.log()
                 .wait()
-        
-    .log( "Subject"         , getVar("Subject")         ) 
-    .log( "StimulusType"    , variable.Stimulus_type    )
-    .log( "Group"           , variable.Group            )                              
-    .log( "Sentence"        , variable.Sentence         )
-    .log( "Item"            , variable.Item             )
-    .log( "Picture1"        , variable.Picture1jpg      )                           
-    .log( "Experiment"      , variable.Correct          ) 
-    .log( "Picture2"        , variable.Picture2jpg      )
+    ,    
+    .log( "Subject"         , getVar("Subject")         ),
+    .log( "StimulusType"    , variable.Stimulus_type    ),
+    .log( "Group"           , variable.Group            ),                             
+    .log( "Sentence"        , variable.Sentence         ),
+    .log( "Item"            , variable.Item             ),
+    .log( "Picture1"        , variable.Picture1jpg      ),                          
+    .log( "Experiment"      , variable.Correct          ),
+    .log( "Picture2"        , variable.Picture2jpg      ),
     .log( "PrimeCondition"  , variable.Prime_condition  )  
     )                              
 )
