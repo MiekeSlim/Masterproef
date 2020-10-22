9aa41e1644161a2b6f5044157427273c8af871f5
HeleneVanMarcke <62547041+HeleneVanMarcke@users.noreply.github.com>
2020-10-22 10:56:29 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -158,7 +158,7 @@ PennController("DeviceCheck+Subject",
 
             ,
             newCanvas(800, 600)
-                .settings.add( 50,     100,      getText("no"))
+                .settings.add( 300,     100,      getText("no"))
                 .settings.add( 500,     100,    getText("yes"))
                 .print()
             ,
